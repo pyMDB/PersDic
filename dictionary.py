bda2ba995a8a2d5147daf179c9b180897dad9076
kasra7 <kasra.ahmadvand@gmail.com>
2014-01-31 21:11:43 +0330

    2thup sourc

--- dictionary.py
+++ dictionary.py
@@ -19,10 +19,18 @@
 #       BY:
 #       MODIFICATION: 
 #
+#
+# set -n  # Uncomment to check your syntax, without execution.
+#         # NOTE: Do not forget to put the comment back in or
+#         #       the shell script will not execute!
+# set -x  # Uncomment to debug this shell script
+#
 ##########################################################
 ########### DEFINE FILES AND VARIABLES:###################
 #Generic_English_Persian.m2 : a free word data base ! 
-#
+#dic1 :
+#a:
+#item:
 ##########################################################
 ##########################################################
 ############### FUNCTIONS: ###################
@@ -43,7 +51,7 @@ item=2
 
 #import data from database
 
-con = lite.connect('data/Generic_English_Persian.m2')
+con = lite.connect('G.m2')
 with con:    
     cur = con.cursor()    
     cur.execute("SELECT * FROM word")
@@ -68,7 +76,7 @@ class InputDialog(QtGui.QWidget):
   lbl3 = QtGui.QLabel('Enter yor word here !', self)
   lbl3.move(134,13)
   self.setGeometry(500,700,500,600)
-  self.setWindowTitle('Tiny Dictionary')
+  self.setWindowTitle('PERSDIC')
   self.button = QtGui.QPushButton('Translate',self)
   self.button.setIconSize(QtCore.QSize(183,30))
   #self.button.setFocusPolicy(QtCore.Qt.NoFocus)
@@ -91,19 +99,19 @@ class InputDialog(QtGui.QWidget):
   self.lbl5.setReadOnly(True)
   self.lbl5.setGeometry(30,150,430,400)
   self.setStyleSheet("QWidget {border:inset;border-radius:3px;color :black;font-weight:500; font-size: 10pt}QPushButton{color:#099000;border-style: outset;border-width: 2px;border-radius: 10px;border-color: beige;font: bold 14px;min-width: 10em;padding: 6px;}QLineEdit{background-color:white; color:black}QTextEdit{background-color:#ffffff; color:#000000}")
-  
-  with open('Words.txt','a+') as f:
+  #bottom = QtGui.QFrame(self)
+  #bottom.setFrameShape(QtGui.QFrame.StyledPanel)
+  #bottom.setGeometry(30,150,430,400)
+  with open('dic/Words.txt','a+') as f:
    b= sum(1 for line in f)
    lbl4 = QtGui.QLabel('['+ str(b)+']'+	' word exist in your dict...!', self)
    lbl4.move(110,125)
    f.close()
-
-#the insert function
  def insert(self):
     flag=True
     pox=2
-    with open('Words.txt','a+') as f:
-     inword=(self.label.text()).toLower()
+    inword=(self.label.text()).toLower()
+    with open('dic/Words.txt','a+') as f:
      for line in f:
       if line==(inword+'\n'):
        self.msg.setText("you have this word \n%s"%dic1[str(inword)].decode('utf8'))
@@ -111,22 +119,23 @@ class InputDialog(QtGui.QWidget):
        #self.label.clear()
        flag=False
        break
-     for i in lito:
-      if i==(inword):
-       if pox>0:
+     if(flag):
+      for i in lito:
+       if i==(inword):
         self.lbl5.clear()
-        pox=-pox
-       self.lbl5.append(dic1[i].decode('utf8'))
-       f.write(inword)
-       f.write('\n')
-       flag=False
-      else:
-       continue
-     if (flag):
+        self.lbl5.append(dic1[i].decode('utf8'))
+        f.write(inword)
+        f.write('\n')
+        flag=False
+        break
+       #self.msg.setText(dic1[i].decode('utf8'))
+       #self.msg.exec_()
+       #self.label.clear()
+     if(flag):
       text,ok=self.dialogbox.getText(QtGui.QInputDialog(),'Create Persian meaning','Enter meaning here: ',QtGui.QLineEdit.Normal,'meaning')
       if ok :  
        dic1[str(inword)]=unicode(text)
-       con = lite.connect('data/Generic_English_Persian.m2')
+       con = lite.connect('G.m2')
        with con:    
         cur = con.cursor()    
         cur.execute("SELECT * FROM word")
@@ -135,16 +144,17 @@ class InputDialog(QtGui.QWidget):
         cur.execute("insert into word (id,Wname,Wmean) values (?, ?, ?)",
             (i,str(inword),unicode(text))) 
         con.commit()
+        #cur.execute("UPDATE word ORDER BY Wname ASC")
        #self.label.clear()
        f.write(inword)
        f.write('\n')
-#update function for update meaning
+
  def update(self):
 
   inword=(self.label.text()).toLower()
   text,ok=self.dialogbox1.getText(QtGui.QInputDialog(),'Update meaning','Enter meaning here: ',QtGui.QLineEdit.Normal,'aaas')
   if ok :
-   con = lite.connect('data/Generic_English_Persian.m2')
+   con = lite.connect('G.m2')
    with con:
     cur = con.cursor()     
     cur.execute("SELECT * FROM word")
@@ -157,7 +167,7 @@ class InputDialog(QtGui.QWidget):
             self.insert()
 
 
-"""class HoverButton(QtGui.QToolButton):
+class HoverButton(QtGui.QToolButton):
 
     def __init__(self, parent=None):
         super(HoverButton, self).__init__(parent)
@@ -170,7 +180,7 @@ class InputDialog(QtGui.QWidget):
 
     def leaveEvent(self,event):
         self.setStyleSheet("background-color:998002;")
-        #print("Leave")"""
+        #print("Leave")
 
 
 app = QtGui.QApplication(sys.argv)
